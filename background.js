3330ea3777120a5abe623cc6352fe19a26e2c6aa
Dane Edwards <dedwards1@costar.com>
2023-12-27 15:00:47 -0500

    fixed template

--- background.js
+++ background.js
@@ -3,30 +3,29 @@ chrome.commands.onCommand.addListener(function(command) {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             const scriptToExec = `
                 var textToPaste = \`## What type of PR is this? (check all applicable)
+- [ ] 📜 Feature
+- [ ] 🐛 Bug Fix
+- [ ] ⚡ Hot-fix
+- [ ] 🔥 Optimization
+- [ ] 🖨️ Documentation Update
+- [ ] 💯 Testing
+- [ ] ↩️ Revert
 
-                - [ ] 📜 Feature
-                - [ ] 🐛 Bug Fix
-                - [ ] ⚡ Hot-fix
-                - [ ] 🔥 Optimization
-                - [ ] 🖨️ Documentation Update
-                - [ ] 💯 Testing
-                - [ ] ↩️ Revert
-                
-                ## Summary
-                
-                ### What was the reason?
-                -
-                
-                ### What is the solution?
-                -
-                
-                ## Added/updated tests?
-                
-                - [ ] Yes
-                - [ ] No, because they aren't needed
-                
-                ### [optional] Screenshots or Recordings showing changes
-                \`;
+## Summary
+
+### What was the reason?
+-
+
+### What is the solution?
+-
+
+## Added/updated tests?
+
+- [ ] Yes
+- [ ] No, because they aren't needed
+
+### [optional] Screenshots or Recordings showing changes
+\`;
                 document.activeElement.value += textToPaste;
             `;
             chrome.tabs.executeScript(tabs[0].id, { code: scriptToExec });
